c382b716054ffab73049e536930a3cb0b2c3d894
stegotraus <jeancaron072001@gmail.com>
2022-11-30 16:10:15 +0100

    cookies + hardware page

--- script.js
+++ script.js
@@ -26,9 +26,78 @@ function darkMode() {
   element.classList.toggle("dark-mode");
 }
   
+var body = document.getElementsByTagName('body')[0];
+var dark_theme_class = 'dark-theme';
+var theme = getCookie('theme');
 
-$.get("navbar.html", function(data){
-  $("#nav-placeholder").replaceWith(data);
+if(theme != '') {
+    body.classList.add(theme);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (getCookie('theme')=='light'){
+        document.getElementById('flexSwitchCheckDefault').checked=false;
+    }
+    else{
+        document.getElementById('flexSwitchCheckDefault').checked=true;
+    }
+
+
+    $('#theme-toggle').on('click', function () {
+
+        if (body.classList.contains(dark_theme_class)) {
+
+            body.classList.remove(dark_theme_class);
+            setCookie('theme', 'light');
+        }
+        else {
+
+            body.classList.add(dark_theme_class);
+
+            setCookie('theme', 'dark-theme');
+            
+        }
+    });
+
 });
+
+// enregistrement du theme dans le cookie
+
+function setCookie(name, value) {
+
+    var d = new Date();
+    d.setTime(d.getTime() + (365*24*60*60*1000));
+    var expires = "expires=" + d.toUTCString();
+    console.log(expires)
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    console.log(document.cookie)
+
+}
+
+
+
+
+// methode de recuperation du theme dans le cookie
+
+function getCookie(cname) {
+
+    var theme = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+
+    for(var i = 0; i < ca.length; i++) {
+
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+
+        if (c.indexOf(theme) == 0) {
+            return c.substring(theme.length, c.length);
+        }
+    }
+    return "";
+}
+
 
 
